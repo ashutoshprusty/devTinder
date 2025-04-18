bd2f0b7f6461f7f661930b581e3e10fb6bd84621
ashutoshprusty <ashutosh.prusty3@gmail.com>
2025-04-18 13:01:13 +0530

    handlers

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
 
+
+app.get('/user', (req, res) => {
+    res.send({fn:'As', ln:'pr'});
+});
+
+app.post('/user', (req, res) => {
+    res.send('Data received');
+});
+app.delete('/user', (req, res) => {
+    res.send('deleted data');
+});
 app.use('/test',(req, res) => {
     res.send('Hello World');
 });
-app.use('/hello',(req, res) => {
-    res.send('Hello World12');
-});
+// app.use('/hello',(req, res) => {
+//     res.send('Hello World12');
+// });
 app.listen(7777, ()=> {
     console.log('Server is running on port 7777');
 } );
